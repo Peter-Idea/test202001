2bb54223cdf948fbd29b11ad1245743449ed6309
praigorodskii <praigorodskii@parseq.pro>
2020-09-17 17:07:08 +0300

    Correct path db

--- src/main/java/ru/rcs/test202001/services/ServiceDatabaseConnect.java
+++ src/main/java/ru/rcs/test202001/services/ServiceDatabaseConnect.java
@@ -3,6 +3,9 @@ package ru.rcs.test202001.services;
 import org.springframework.stereotype.Service;
 import org.sqlite.JDBC;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -10,13 +13,17 @@ import java.sql.SQLException;
 @Service
 public class ServiceDatabaseConnect {
 
-    private static final String CON_STR = "jdbc:sqlite:dataset/data.sqlite";
+    private  String dirPath = "/usr/local/lib/dataset/data.sqlite";
+    private  File file = new File(dirPath);
+    private final String CON_STR = "jdbc:sqlite:"+file.getAbsolutePath();
 
     private Connection connection;
 
     public ServiceDatabaseConnect() throws SQLException {
         DriverManager.registerDriver(new JDBC());
         this.connection = DriverManager.getConnection(CON_STR);
+        System.out.println(file.getPath()+" "+file.getAbsolutePath());
+
     }
 
     public Connection getConnection() {
